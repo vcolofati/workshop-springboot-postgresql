4bca8125b907c52e8eae9fe550ecccacc2f117ca
Victor <vcolofati@gmail.com>
2021-04-06 11:12:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/example/victor/course/services/UserService.java
@@ -0,0 +1,24 @@
+package com.example.victor.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.victor.course.entities.User;
+import com.example.victor.course.repositories.UserRepository;
+
+@Service
+public class UserService  {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		return repository.findById(id).get();
+	}
+}
