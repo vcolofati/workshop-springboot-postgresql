ec36b4f6c5f55106d832afefc2e8f8ec3c06b6af
Victor <vcolofati@gmail.com>
2021-04-07 11:24:30 -0300

    Exception handling - findById

---
+++ src/main/java/com/example/victor/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.victor.course.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.victor.course.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest req) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), req.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/example/victor/course/services/UserService.java
+++ src/main/java/com/example/victor/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.victor.course.entities.User;
 import com.example.victor.course.repositories.UserRepository;
+import com.example.victor.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService  {
@@ -19,7 +20,7 @@ public class UserService  {
 	}
 	
 	public User findById(Long id) {
-		return repository.findById(id).get();
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
